35b68bbf209fd1d35c862e82104e6f04c7ea0b11
soywiz <soywiz@gmail.com>
2020-03-15 16:54:06 +0100

    Update versions

--- dividead.main.kts
+++ dividead.main.kts
@@ -1,18 +1,37 @@
-#!/usr/bin/env kscript
-import com.soywiz.korio.*
+#!/usr/bin/env kotlin
+
+@file:Repository("https://jcenter.bintray.com/")
+@file:Repository("https://dl.bintray.com/korlibs/korlibs/")
+@file:DependsOn("com.soywiz.korlibs.korio:korio-jvm:1.10.0")
+@file:DependsOn("com.soywiz.korlibs.korim:korim-jvm:1.10.0")
+@file:DependsOn("com.offbytwo:docopt:0.6.0.20150202")
+@file:DependsOn("log4j:log4j:1.2.14")
+//@file:CompilerOptions()
+//@file:Import()
+
+// https://github.com/Kotlin/KEEP/blob/master/proposals/scripting-support.md#kotlin-main-kts
+// To install kotlin:
+//   curl -s https://get.sdkman.io | bash
+//   source "$HOME/.sdkman/bin/sdkman-init.sh"
+//   sdk install kotlin
+
+import com.soywiz.kmem.*
 import com.soywiz.korio.async.*
-import com.soywiz.korio.error.*
+import com.soywiz.korio.file.*
+import com.soywiz.korio.file.std.*
+import com.soywiz.korio.lang.*
 import com.soywiz.korio.stream.*
-import com.soywiz.korio.util.*
-import com.soywiz.korio.vfs.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import org.docopt.*
 import java.util.*
+import kotlin.collections.Iterable
+import kotlin.collections.contains
+import kotlin.collections.map
+import kotlin.collections.set
+import kotlin.collections.sliceArray
+import kotlin.collections.toList
 
-// https://github.com/holgerbrandl/kscript
-
-//DEPS com.soywiz:korio:0.19.1
-//DEPS com.soywiz:korim:0.19.1
-//DEPS com.offbytwo:docopt:0.6.0.20150202 log4j:log4j:1.2.14
 val args2: Array<String> = args
 
 val usage = """
@@ -29,7 +48,7 @@ Options:
 val docopt = Docopt(usage)
 val params = docopt.parse(args2.toList())
 
-syncTest {
+runBlocking {
     when {
         params["-l"] == true -> {
             val file = params["<file.dl1>"]!!.toString()
@@ -72,8 +91,8 @@ class DL1 : Vfs() {
             // Read header
             val header = stream.readBytesExact(0x10).openAsync()
             val magic = header.readStringz(8)
-            val count = header.readU16_le()
-            val offset = header.readS32_le()
+            val count = header.readU16LE()
+            val offset = header.readS32LE()
             var pos = 0x10
 
             println("Loading entries from DL1 $count: $offset")
@@ -86,7 +105,7 @@ class DL1 : Vfs() {
             val entriesByteArray = it.openSync()
             for (n in 0 until count) {
                 val name: String = entriesByteArray.readStringz(12)
-                val size: Int = entriesByteArray.readS32_le()
+                val size: Int = entriesByteArray.readS32LE()
                 dl1.entries[name.toUpperCase()] = stream.sliceWithSize(pos.toLong(), size.toLong())
                 pos += size
             }
@@ -94,8 +113,8 @@ class DL1 : Vfs() {
         }
     }
 
-    override suspend fun list(path: String): SuspendingSequence<VfsFile> {
-        return this.entries.map { this.file(it.key) }.toAsync()
+    override suspend fun list(path: String): ReceiveChannel<VfsFile> {
+        return this.entries.map { this.file(it.key) }.toChannel()
     }
 
     override suspend fun stat(path: String): VfsStat {
@@ -131,8 +150,8 @@ object LZ {
     fun decompress(data: ByteArray): ByteArray {
         val data = data.openSync()
         val magic = data.readStringz(2)
-        var compressedSize = data.readS32_le()
-        val uncompressedSize = data.readS32_le()
+        var compressedSize = data.readS32LE()
+        val uncompressedSize = data.readS32LE()
         if (magic != "LZ") throw InvalidOperationException("Invalid LZ stream")
         return _decode(data, uncompressedSize)
     }
@@ -157,7 +176,7 @@ object LZ {
 
     // @:noStack
     private fun _decodeFast(input: SyncStream, uncompressedSize: Int): ByteArray {
-        val i = (input.base as MemorySyncStreamBase).data.data
+        val i = UByteArrayInt((input.base as MemorySyncStreamBase).data.data)
         var ip: Int = input.position.toInt()
         val il: Int = input.length.toInt()
 
@@ -166,18 +185,18 @@ object LZ {
         val ringStart = 0xFEE
 
         while (ip < il) {
-            var code = i.getu(ip++) or 0x100
+            var code = i[ip++] or 0x100
 
             while (code != 1) {
                 // Uncompressed
                 if ((code and 1) != 0) {
-                    o[op++] = i[ip++]
+                    o[op++] = i[ip++].toByte()
                 }
                 // Compressed
                 else {
                     if (ip >= il) break
-                    val paramL = i.getu(ip++)
-                    val paramH = i.getu(ip++)
+                    val paramL = i[ip++]
+                    val paramH = i[ip++]
                     val param = paramL or (paramH shl 8)
                     val ringOffset = extractPosition(param)
                     val ringLength = extractCount(param)
